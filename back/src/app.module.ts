ad1b007a739b48c15ea522be4f96ba5c446988c4
robergem2u <c.robergementec@outlook.fr>
2021-11-29 14:15:55 +0100

    import DiscussionModule in AppModule

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -5,10 +5,11 @@ import {MongooseModule} from "@nestjs/mongoose";
 import { PeopleModule } from './people/people.module';
 import { AuthModule } from './auth/auth.module';
 import * as Config from 'config'
+import {DiscussionModule} from "./discussion/discussion.module";
 
 
 @Module({
-  imports: [MongooseModule.forRoot(Config.get<string>('mongodb.uri')), PeopleModule, AuthModule],
+  imports: [MongooseModule.forRoot(Config.get<string>('mongodb.uri')), PeopleModule, AuthModule, DiscussionModule],
   controllers: [AppController],
   providers: [AppService],
 })
