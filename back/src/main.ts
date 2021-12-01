b4228415ecb2200bd798138ba6d356e7d949d329
jordan <scherring.jordan@gmail.com>
2021-12-01 19:45:45 +0100

    correction ajout jeux

--- back/src/main.ts
+++ back/src/main.ts
@@ -10,11 +10,17 @@ import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { PeopleModule } from './people/people.module';
 import { GamesModule } from './games/games.module';
+import { DiscussionsModule } from './discussions/discussions.module';
+import { PostModule } from './post/post.module';
+import { ActualitesModule } from './actualites/actualites.module';
 
 async function bootstrap(
   config: AppConfig,
   swaggerConfig: SwaggerConfig,
   swaggerConfigGames: SwaggerConfig,
+  swaggerConfigDiscussions: SwaggerConfig,
+  swaggerConfigPosts: SwaggerConfig,
+  swaggerConfigActualites: SwaggerConfig,
 ) {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
@@ -57,10 +63,54 @@ async function bootstrap(
 
   SwaggerModule.setup(swaggerConfigGames.path, app, gamesDocument);
 
+  const thirdoptions = new DocumentBuilder()
+    .setTitle(swaggerConfigDiscussions.title)
+    .setDescription(swaggerConfigDiscussions.description)
+    .setVersion(swaggerConfigDiscussions.version)
+    .addTag(swaggerConfigDiscussions.tag)
+    .build();
+
+  const discussionsDocument = SwaggerModule.createDocument(app, thirdoptions, {
+    include: [DiscussionsModule],
+  });
+
+  SwaggerModule.setup(swaggerConfigDiscussions.path, app, discussionsDocument);
+
+  const fourthoptions = new DocumentBuilder()
+    .setTitle(swaggerConfigPosts.title)
+    .setDescription(swaggerConfigPosts.description)
+    .setVersion(swaggerConfigPosts.version)
+    .addTag(swaggerConfigPosts.tag)
+    .build();
+
+  const postsDocument = SwaggerModule.createDocument(app, fourthoptions, {
+    include: [PostModule],
+  });
+
+  SwaggerModule.setup(swaggerConfigPosts.path, app, postsDocument);
+
+
+  const fifthoptions = new DocumentBuilder()
+    .setTitle(swaggerConfigActualites.title)
+    .setDescription(swaggerConfigActualites.description)
+    .setVersion(swaggerConfigActualites.version)
+    .addTag(swaggerConfigActualites.tag)
+    .build();
+
+  const actualitesDocument = SwaggerModule.createDocument(app, fifthoptions, {
+    include: [ActualitesModule],
+  });
+
+  SwaggerModule.setup(swaggerConfigActualites.path, app, actualitesDocument);
+
   await app.listen(config.port, config.host);
 }
+
 bootstrap(
   Config.get<AppConfig>('server'),
   Config.get<SwaggerConfig>('swagger_people'),
   Config.get<SwaggerConfig>('swagger_games'),
+  Config.get<SwaggerConfig>('swagger_discussions'),
+  Config.get<SwaggerConfig>('swagger_posts'),
+  Config.get<SwaggerConfig>('swagger_actualites'),
 );
